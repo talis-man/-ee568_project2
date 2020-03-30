22d57fec770f0d6221d43d6028c144f5ae437edd
talis-man <39023072+talis-man@users.noreply.github.com>
2020-03-30 09:57:25 +0300

    winding factor is added 0.9577

--- first_24.m
+++ first_24.m
@@ -1,4 +1,3 @@
-
 close all
 clear all
 clc
@@ -6,40 +5,66 @@ clc
 %24Slot-22Pole-First Harmonic
 M = cos(0*pi/180)
 N = sin(0*pi/180)
+ver = M
+hor = N
 figure(1)
 compass(M)
 hold on
+
 M = cos(165*pi/180)
 N = sin(165*pi/180)
+ver = ver - M
+hor = hor -N
 figure(1)
 compass(M, N)
 hold on
+
 M = cos(330*pi/180)
 N = sin(330*pi/180)
+ver = ver + M
+hor = hor +N
 figure(1)
 compass(M, N)
 hold on
+
 M = cos(135*pi/180)
 N = sin(135*pi/180)
+ver = ver - M
+hor = hor -N
 figure(1)
 compass(M, N)
 hold on
+
 M = cos(180*pi/180)
 N = sin(180*pi/180)
+ver = ver - M
+hor = hor -N
 figure(1)
 compass(M, N)
 hold on
+
 M = cos(345*pi/180)
 N = sin(345*pi/180)
+ver = ver + M
+hor = hor +N
 figure(1)
 compass(M, N)
 hold on
+
 M = cos(150*pi/180)
 N = sin(150*pi/180)
+ver = ver - M
+hor = hor -N
 figure(1)
 compass(M, N)
 hold on
+
 M = cos(315*pi/180)
 N = sin(315*pi/180)
+ver = ver + M
+hor = hor +N
 figure(1)
 compass(M, N)
+
+mag = sqrt(hor^2 + ver^2)
+w = mag/8
