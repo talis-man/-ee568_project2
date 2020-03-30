ce389e860ddc07c0d89220a0ae40e83bd31c0d3b
talis-man <39023072+talis-man@users.noreply.github.com>
2020-03-30 10:34:46 +0300

    winding factor is added 0.1000

--- fifth_30.m
+++ fifth_30.m
@@ -1,55 +1,86 @@
-
 close all
 clear all
 clc
 
-%24Slot-22Pole-First Harmonic
+%30Slot-22Pole-First Harmonic
 M = cos(0*pi/180)
 N = sin(0*pi/180)
+ver = M
+hor = N
 figure(1)
 compass(M,N)
 hold on
+
 M = cos(300*pi/180)
 N = sin(300*pi/180)
+ver = ver - M
+hor = hor - N
 figure(1)
 compass(M, N)
 hold on
+
 M = cos(120*pi/180)
 N = sin(120*pi/180)
+ver = ver - M
+hor = hor -N
 figure(1)
 compass(M, N)
 hold on
+
 M = cos(60*pi/180)
 N = sin(60*pi/180)
+ver = ver +M
+hor = hor +N
 figure(1)
 compass(M, N)
 hold on
+
 M = cos(240*pi/180)
 N = sin(240*pi/180)
+ver = ver + M
+hor = hor +N
 figure(1)
 compass(M, N)
 hold on
+
 M = cos(180*pi/180)
 N = sin(180*pi/180)
+ver = ver - M
+hor = hor -N
 figure(1)
 compass(M, N)
 hold on
+
 M = cos(120*pi/180)
 N = sin(120*pi/180)
+ver = ver + M
+hor = hor +N
 figure(1)
 compass(M, N)
 hold on
+
 M = cos(60*pi/180)
 N = sin(60*pi/180)
+ver = ver - M
+hor = hor -N
 figure(1)
 compass(M, N)
 hold on
+
 M = cos(240*pi/180)
 N = sin(240*pi/180)
+ver = ver - M
+hor = hor -N
 figure(1)
 compass(M, N)
 hold on
+
 M = cos(180*pi/180)
 N = sin(180*pi/180)
+ver = ver + M
+hor = hor + N
 figure(1)
 compass(M, N)
+
+mag = sqrt(hor^2 + ver^2)
+w = mag/10
